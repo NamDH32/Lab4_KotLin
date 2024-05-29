7438a34a332cbe91dcd6a886a36359c8a91d316e
NamDH32 <132922152+NamDH32@users.noreply.github.com>
2024-05-30 06:38:19 +0700

    Lab5

--- app/src/main/java/com/dhnph34455/fpt/edu/lab4_kotlin/MainActivity.kt
+++ app/src/main/java/com/dhnph34455/fpt/edu/lab4_kotlin/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,7 +20,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,12 +34,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +49,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginScreen(context = this)
+        }
+    }
+}
+
+@Composable
+fun LoginApp() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center,
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            colors = CardDefaults.cardColors(containerColor =
+            Color.White),
+            elevation = CardDefaults.cardElevation(defaultElevation =
+            4.dp)
+        ) {
+            LoginScreen(context = LocalContext.current)
         }
     }
 }
@@ -83,6 +115,8 @@ fun LoginScreen(context: Context) {
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             modifier = Modifier.fillMaxWidth()
         )
+        RememberMeSwitch()
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -104,6 +138,63 @@ fun LoginScreen(context: Context) {
             Text(text = "Login")
         }
 
+    }
+}
+@Composable
+fun RememberMeSwitch() {
+    var isChecked by remember { mutableStateOf(false) }
+    Row(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it }
+        )
+        Text("Remember Me?", modifier = Modifier.padding(start =
+        12.dp))
+    }
+}
+
+@Composable
+fun DialogComponent(
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogMessage: String,
+) {
+    Dialog(onDismissRequest = {}) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+            modifier = Modifier.padding(20.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Text(dialogTitle, style =
+                MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(dialogMessage, style =
+                MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = onConfirmation,
+
+                    modifier = Modifier.align(Alignment.End),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.DarkGray,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Okay")
+                }
+            }
+        }
     }
 }
 @Preview(showBackground = true, showSystemUi = true)
---
+++ app/src/main/java/com/dhnph34455/fpt/edu/lab4_kotlin/ui/theme/Bai2lab5.kt
@@ -0,0 +1,76 @@
+package com.dhnph34455.fpt.edu.lab4_kotlin.ui.theme
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dhnph34455.fpt.edu.lab4_kotlin.R
+
+class Bai2lab5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LightSwitch()
+        }
+    }
+
+    @Composable
+    fun LightSwitch() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val isChecked = remember { mutableStateOf(false) }
+            if (isChecked.value) {
+                Image(
+                    painter = painterResource(id = R.drawable.bulb_on),
+                    contentDescription = "Light is On",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.bulb_off),
+                    contentDescription = "Light is Off",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            Switch(
+                checked = isChecked.value,
+                onCheckedChange = { isChecked.value = it },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = Color.Green,
+                    uncheckedThumbColor = Color.Gray,
+                    checkedTrackColor = Color.Green.copy(alpha = 0.5f),
+                    uncheckedTrackColor = Color.Gray.copy(alpha = 0.5f),
+                    checkedBorderColor = Color.Green.copy(alpha = 0.75f)
+                )
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun LightSwitchPreview() {
+        LightSwitch()
+    }
+}
